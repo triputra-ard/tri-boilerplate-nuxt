ac3db44917b16074473cbd3e304b1273870261d1
Tri Putra Ard. <trieputra@live.com>
2025-01-10 18:07:48 +0700

    feat: add new string function

--- utils/stringConverter.ts
+++ utils/stringConverter.ts
@@ -1,11 +1,11 @@
 export default function () {
-  const convertToKebabCase = (value: String) => {
+  const convertToKebabCase = (value: string) => {
     return value
       .replace(/([a-z])([A-Z])/g, "$1-$2") // Add hyphen between camelCase words
       .replace(/[\s_]+/g, "-") // Replace spaces and underscores with hyphen
       .toLowerCase(); // Convert to lowercase
   };
-  const convertToSnakeCase = (value: String) => {
+  const convertToSnakeCase = (value: string) => {
     return value.toLowerCase().replace(/\s+/g, "_").replace(/&/g, "x");
   };
   const convertToCamelCase = (value: String) => {
@@ -18,11 +18,19 @@ export default function () {
       )
       .join("");
   };
-  const convertToNormalString = (value: String) => {
+  const convertToNormalString = (value: string) => {
     return value
       .replace(/[-_]/g, " ") // Replace - and _ with space
       .replace(/([a-z])([A-Z])/g, "$1 $2") // Add space before capital letters
       .toLowerCase();
+  };
+  const capitalizeWords = (string: string) => {
+    if (!str || typeof str !== "string") return "";
+
+    return str
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(" ");
   };
 
   return {
@@ -30,5 +38,6 @@ export default function () {
     convertToSnakeCase,
     convertToCamelCase,
     convertToNormalString,
+    capitalizeWords,
   };
 }
